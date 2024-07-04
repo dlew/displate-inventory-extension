7423063b106a53f0a620082a6e34958452e3f3bd
Njitram <dev@njitram.com>
2024-07-04 20:16:22 +0200

    Update "more" slider with current LEs on PDP too

--- content-script.js
+++ content-script.js
@@ -13,6 +13,7 @@
  */ 
 
 let LE_LIST_SELECT = "[class^=LimitedEditionListSection_list__]";
+let PRODUCT_SLIDER_MORE_TILES = ".product-slider--more .displate-tile--limited";
 let PRODUCT_PAGE_BOX_SELECT = ".product-page__product-box";
 
 let loadAndShowLimitedEditionData = function () {
@@ -30,26 +31,21 @@ let loadAndShowLimitedEditionData = function () {
             return true;
           }
         }
-        // List page: Find tiles and add to them
-        else {
-          let tiles = findLimitedEditionTiles();
-          if(!tiles.length) {
-            return false;
+        // List page & more slider on PDP: Find tiles and add to them
+        let tiles = findLimitedEditionTiles();
+        tiles.forEach(tile => {
+          // Do not update a tile twice since all tiles will be looped over every time more are added
+          if(updatedTiles.filter(elem => elem === tile).length) {
+            return;
           }
-          tiles.forEach(tile => {
-            // Do not update a tile twice since all tiles will be looped over every time more are added
-            if(updatedTiles.filter(elem => elem === tile).length) {
-              return;
-            }
-  
-            let tileData = findLimitedEditionDataForTile(data, tile)
-            reformatTile(tile, tileData)
-            addInventoryDataToTile(tile, tileData)
-  
-            updatedTiles.push(tile);
-          })
-          return true;
-        }
+
+          let tileData = findLimitedEditionDataForTile(data, tile)
+          reformatSoldOutTile(tile, tileData)
+          addInventoryDataToTile(tile, tileData)
+
+          updatedTiles.push(tile);
+        })
+        return true;
       })
 }
 
@@ -109,7 +105,7 @@ let getProductPageProductBox = function() {
 }
 
 let findLimitedEditionTiles = function() {
-  return [...document.querySelectorAll(LE_LIST_SELECT + " > div")]
+  return [...document.querySelectorAll(`${LE_LIST_SELECT} > div , ${PRODUCT_SLIDER_MORE_TILES}`)]
 }
 
 let hasLimitedEditionTiles = function() {
@@ -118,14 +114,14 @@ let hasLimitedEditionTiles = function() {
 
 let findLimitedEditionDataForTile = function(data, tile) {
   // Unreleased LEs
-  if (tile.querySelector('[class^=LimitedCountdown]')) {
+  if (tile.querySelector('[class^=LimitedCountdown]') || tile.classList.contains('displate-tile--limited-upcoming')) {
     let title = tile.querySelector("h5").innerHTML
     return data.find(element => element.title == title)
   }
   // Current or past LEs
   else {
     // Bug: sometimes this querySelector returns null and then throws an error. But on page refresh, it does not happen. Race condition?
-    let url = new URL(tile.querySelector('a').href)
+    let url = new URL(tile.href || tile.querySelector('a').href)
     let pathnames = url.pathname.split("/")
     let itemCollectionId = parseInt(pathnames[pathnames.length - 1])
     return data.find(element => element.itemCollectionId == itemCollectionId)
@@ -133,7 +129,7 @@ let findLimitedEditionDataForTile = function(data, tile) {
 }
 
 // Makes sold out tiles prettier (IMO)
-let reformatTile = function(tile, tileData) {
+let reformatSoldOutTile = function(tile, tileData) {
   let soldOutElem = tile.querySelector('[class^=SoldOut_container__]')
   if (soldOutElem) {
     // Remove sold out overlay
@@ -174,7 +170,7 @@ let addInventoryDataToTile = function(tile, tileData) {
   div.appendChild(p)
 
   // Insert before pulsometer (if present), otherwise just append to end
-  let pulsometer = tile.querySelector("[class^=Pulsometer_container__]")
+  let pulsometer = tile.querySelector("[class^=Pulsometer_container__], .editions__pulsometer")
   if (pulsometer != null) {
     tile.insertBefore(div, pulsometer)
   }
@@ -227,9 +223,11 @@ window.addEventListener("popstate", function (event) {
 });
 
 let currentPath = new URL(window.location.href).pathname;
+let isListPage = currentPath == "/limited-edition";
+let isProductPage = currentPath.startsWith("/limited-edition/displate");
 
 // LE list page
-if (currentPath == "/limited-edition") {
+if (isListPage) {
   waitForElement(LE_LIST_SELECT).then((leListElem) => {
     // Initial content edit
     loadAndShowLimitedEditionData();
@@ -245,10 +243,12 @@ if (currentPath == "/limited-edition") {
     )
   });
 }
-
 // LE product page
- else if (currentPath.startsWith("/limited-edition/displate")) {
+else if (currentPath.startsWith("/limited-edition/displate")) {
   waitForElement(PRODUCT_PAGE_BOX_SELECT).then(() => {
+    loadAndShowLimitedEditionData()
+  })
+  waitForElement(PRODUCT_SLIDER_MORE_TILES).then(() => {
     loadAndShowLimitedEditionData()
   })
 }
