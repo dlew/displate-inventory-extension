2377e8ba683237c18ced06953dd72c1dc71feca1
Dan Lew <danlew42@gmail.com>
2024-07-10 09:24:38 -0500

    Rewrote content-script to be TypeScript/ESLint compatible

--- src/content-script.ts
+++ src/content-script.ts
@@ -1,7 +1,3 @@
-/* eslint-disable */
-// @ts-nocheck
-// (Going to refactor all of this to be TypeScript eventually, but no need to fix anything yet)
-
 import { fetchAllLimitedEditionData } from "./api";
 import { reformatPage } from "./dom-manipulators";
 
@@ -19,16 +15,17 @@ import { reformatPage } from "./dom-manipulators";
  * 3. Once we have that, find all tiles and improve them.
  */
 
-let LE_LIST_SELECT = "[class^=LimitedEditionListSection_list__]";
-let PRODUCT_SLIDER_MORE_TILES = ".product-slider--more .displate-tile--limited";
-let PRODUCT_PAGE_BOX_SELECT = ".product-page__product-box";
+const LE_LIST_SELECT = "[class^=LimitedEditionListSection_list__]";
+const PRODUCT_SLIDER_MORE_TILES =
+  ".product-slider--more .displate-tile--limited";
+const PRODUCT_PAGE_BOX_SELECT = ".product-page__product-box";
 
 async function loadAndShowLimitedEditionData() {
   const data = await fetchAllLimitedEditionData();
   reformatPage(document, data);
 }
 
-let waitForElement = (selector) => {
+function waitForElement(selector: string): Promise<Element> {
   return new Promise((resolve) => {
     // Does it already exist?
     const targetElem = document.querySelector(selector);
@@ -36,8 +33,8 @@ let waitForElement = (selector) => {
       return resolve(targetElem);
     }
 
-    // Wait for it to be added (eg by react)
-    const observer = new MutationObserver((mutations) => {
+    // Wait for it to be added (e.g. by react)
+    const observer = new MutationObserver(() => {
       const targetElem = document.querySelector(selector);
       if (targetElem) {
         // Clean up observer
@@ -52,48 +49,52 @@ let waitForElement = (selector) => {
       subtree: true,
     });
   });
-};
-
-let updatedTiles = [];
+}
 
 // Event listener to handle back or forward navigation:
 // - Removes the 'added-inv-data' class to allow re-adding inventory data.
 // - Removes all elements with the 'units' class.
-window.addEventListener("popstate", function (event) {
-  let elements = document.querySelectorAll(".added-inv-data");
+window.addEventListener("popstate", function () {
+  const elements = document.querySelectorAll(".added-inv-data");
   elements.forEach((element) => element.classList.remove("added-inv-data"));
 
-  let unitsElements = document.querySelectorAll(".units");
-  unitsElements.forEach((element) => element.parentNode.removeChild(element));
+  const unitsElements = document.querySelectorAll(".units");
+  unitsElements.forEach((element) => element.parentNode?.removeChild(element));
 });
 
-let currentPath = new URL(window.location.href).pathname;
-let isListPage = currentPath == "/limited-edition";
-let isProductPage = currentPath.startsWith("/limited-edition/displate");
+const currentPath = new URL(window.location.href).pathname;
+const isListPage = currentPath == "/limited-edition";
 
-// LE list page
-if (isListPage) {
-  waitForElement(LE_LIST_SELECT).then((leListElem) => {
+async function main() {
+  // LE list page
+  if (isListPage) {
+    const leListElem = await waitForElement(LE_LIST_SELECT);
+
     // Initial content edit
-    loadAndShowLimitedEditionData();
+    await loadAndShowLimitedEditionData();
+
     // Edit all content added later too
     // Also, listen to changes to the DOM to find new LE tiles
-    let observer = new MutationObserver(() => {
-      loadAndShowLimitedEditionData();
+    const observer = new MutationObserver(async () => {
+      await loadAndShowLimitedEditionData();
     });
+
     observer.observe(
       leListElem,
       // All tiles are direct children of leListElem so setting subtree to false ensures only these are selected
       { childList: true, subtree: false },
     );
-  });
+  }
+  // LE product page
+  else if (currentPath.startsWith("/limited-edition/displate")) {
+    await Promise.all([
+      waitForElement(PRODUCT_PAGE_BOX_SELECT),
+      waitForElement(PRODUCT_SLIDER_MORE_TILES),
+    ]);
+    await loadAndShowLimitedEditionData();
+  }
 }
-// LE product page
-else if (currentPath.startsWith("/limited-edition/displate")) {
-  waitForElement(PRODUCT_PAGE_BOX_SELECT).then(() => {
-    loadAndShowLimitedEditionData();
-  });
-  waitForElement(PRODUCT_SLIDER_MORE_TILES).then(() => {
-    loadAndShowLimitedEditionData();
-  });
-}
+
+(async () => {
+  await main();
+})();
