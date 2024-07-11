5e57245a624870a19d4e602ab135369830519a33
Dan Lew <danlew42@gmail.com>
2024-07-11 11:06:26 -0500

    Greatly simplified loading & navigation flow
    Now, we *always* listen for new content and update the page based on it.
    This is how the codebase was in the past, we'd gotten away from it a bit.
    
    So, we no longer care about what page we're on - if we detect any LE
    content, we try to modify it.

--- src/content-script.ts
+++ src/content-script.ts
@@ -1,7 +1,9 @@
 import { fetchAllLimitedEditionData } from "./api";
 import { clearFormatting, reformatPage } from "./dom-manipulators";
-import { waitForPageElement } from "./dom-query";
-import { PageElement } from "./model/page-element";
+import {
+  hasLimitedEditionElements,
+  observeNewLimitedEditions,
+} from "./dom-query";
 
 /**
  * This script improves the data shown for Displate Limited Editions.
@@ -17,48 +19,26 @@ import { PageElement } from "./model/page-element";
  * 3. Once we have that, find all tiles and improve them.
  */
 async function main() {
-  const currentPath = new URL(window.location.href).pathname;
+  // Event listener to handle back or forward navigation
+  window.addEventListener("popstate", function () {
+    clearFormatting(document);
+  });
 
-  // LE list page
-  if (currentPath == "/limited-edition") {
-    const leListElem = await waitForPageElement(PageElement.LimitedEditionList);
-
-    // Initial content edit
-    await loadAndShowLimitedEditionData();
-
-    // Edit all content added later too
-    // Also, listen to changes to the DOM to find new LE tiles
-    const observer = new MutationObserver(async () => {
-      await loadAndShowLimitedEditionData();
-    });
-
-    observer.observe(
-      leListElem,
-      // All tiles are direct children of leListElem so setting subtree to false ensures only these are selected
-      { childList: true, subtree: false },
-    );
-  }
-  // LE product page
-  else if (currentPath.startsWith("/limited-edition/displate")) {
-    await Promise.all([
-      waitForPageElement(PageElement.ProductPageBox),
-      waitForPageElement(PageElement.ProductSliderTiles),
-    ]);
+  // Check if page already has LE elements, if so then show LE data
+  if (hasLimitedEditionElements(document)) {
     await loadAndShowLimitedEditionData();
   }
+
+  // Displate's site dynamically loads content as pages load & during navigation
+  observeNewLimitedEditions(document, async () => {
+    await loadAndShowLimitedEditionData();
+  });
 }
 
 async function loadAndShowLimitedEditionData() {
   const data = await fetchAllLimitedEditionData();
   reformatPage(document, data);
 }
-
-// Event listener to handle back or forward navigation:
-// - Removes the 'added-inv-data' class to allow re-adding inventory data.
-// - Removes all elements with the 'units' class.
-window.addEventListener("popstate", function () {
-  clearFormatting(document);
-});
 
 // Start the script
 (async () => {
--- src/model/page-element.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Represents a part of the DOM that we have particular interest in
- */
-export enum PageElement {
-  LimitedEditionList,
-  ProductSliderTiles,
-  ProductPageBox,
-}
